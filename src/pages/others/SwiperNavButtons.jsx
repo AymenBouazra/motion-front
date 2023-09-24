12c17ea85d508b9e7bac657e6c5f32a2b88ff2b7
Rachoud32 <rachoud32@gmail.com>
2023-09-24 08:26:52 +0100

    fix classname swiper nav

--- src/pages/others/SwiperNavButtons.jsx
+++ src/pages/others/SwiperNavButtons.jsx
@@ -7,10 +7,10 @@ export const SwiperNavButtons = () => {
   return (
      <div className="main-slider__nav">
         <div className="swiper-button-prev" id="main-slider__swiper-button-prev" onClick={() => swiper.slidePrev()}>
-        <span class="icon-right-arrow icon-left-arrow"></span>
+        <span className="icon-right-arrow icon-left-arrow"></span>
         </div>
         <div className="swiper-button-next" id="main-slider__swiper-button-next" onClick={() => swiper.slideNext()}>
-        <span class="icon-right-arrow"></span>
+        <span className="icon-right-arrow"></span>
         </div>
     </div>
   );
